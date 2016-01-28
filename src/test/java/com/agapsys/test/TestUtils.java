a5d7b9503807a24c18c350c33d7f60db78f3b854
Leandro Oliveira <leandro@agapsys.com>
2016-01-27 21:25:06 -0300

    Removed unused code

--- src/test/java/com/agapsys/test/TestUtils.java
+++ src/test/java/com/agapsys/test/TestUtils.java
@@ -15,10 +15,7 @@
  */
 package com.agapsys.test;
 
-import com.agapsys.http.HttpClient;
-import com.agapsys.http.HttpGet;
 import com.agapsys.http.HttpResponse;
-import com.agapsys.sevlet.test.ServletContainer;
 import javax.servlet.http.HttpServletResponse;
 import org.junit.Assert;
 
@@ -40,36 +37,5 @@ public class TestUtils {
 	public static void assertStringResponse(int expectedStatus, String expectedContent, HttpResponse.StringResponse resp) {
 		assertStatus(expectedStatus, resp);
 		Assert.assertEquals(expectedContent, resp.getContentString());
-	}
-	
-	public static enum LoginType {
-		SIMPLE("/session/doSimpleLogin", "/csrf/doSimpleLogin"),
-		EXTRA("/session/doExtraLogin", "/csrf/doSimpleLogin"),
-		ADMIN("/session/doAdminLogin", "/csrf/doSimpleLogin");
-		
-		private final String sessionUri;
-		private final String csrfUri;
-		
-		private LoginType(String uri, String csrfUri) {
-			this.sessionUri = uri;
-			this.csrfUri = csrfUri;
-		}
-		
-		public String getSessionUri() {
-			return sessionUri;
-		}
-
-		public String getCsrfUri() {
-			return csrfUri;
-		}
-	}
-	
-	public static HttpClient doSessionLogin(ServletContainer sc, LoginType loginType) {
-		if (loginType == null)
-			throw new IllegalArgumentException("Login type must be informed");
-		
-		HttpClient client = new HttpClient();
-		sc.doRequest(client, new HttpGet(loginType.getSessionUri()));
-		return client;
 	}
 }
